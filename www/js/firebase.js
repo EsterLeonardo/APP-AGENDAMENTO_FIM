2a245afa8ef8c1fc84cb0d25ff827111d9658f5d
Ester Leonardo <ester.leonardo@etec.sp.gov.br>
2021-10-26 22:16:52 -0300

    commit message

---
+++ www/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCOMe3bJ3TefhhmlL5s-UpfzytVutdpd3o",
+  authDomain: "agendamentos-a6762.firebaseapp.com",
+  projectId: "agendamentos-a6762",
+  storageBucket: "agendamentos-a6762.appspot.com",
+  messagingSenderId: "1014091270660",
+  appId: "1:1014091270660:web:6ab370a8c4dd6b5b354351",
+  measurementId: "G-W932KQY8Y3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
---
+++ www/js/index.js
@@ -0,0 +1,43 @@
+var app = {
+        
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {
+        document.getElementById("btnInserir").addEventListener("click",app.inserir);
+    },
+
+    inserir: function(){
+        event.preventDefault();
+        
+        let cnome = document.getElementById("txtNome").value;
+        let ctelefone = document.getElementById("txtTelefone").value;
+        let corigem = document.getElementById("txtOrigem").value;
+        let cdata_contato = document.getElementById("txtDataContato").value;
+        let cobservacao = document.getElementById("txtObservacao").value;
+
+
+        var db = firebase.firestore();
+
+        db.collection("agendamentos").add({
+            nome: cnome,
+            telefone: ctelefone,
+            origem: corigem,
+            data_contato: cdata_contato,
+            observacao: cobservacao,
+        })
+        .then((docRef) =>{
+            console.log("Document written with ID: ", docRef.id);
+            windows.location.href = cordova.file.applicationDirectory + "www/index.html";
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+
+    }
+    
+};
+
+app.initialize();
